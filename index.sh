953f14dd14d9b84560e1bbe99dc188a7ab416b3e
Amheex ツ <amheex@gmail.com>
2026-09-24 20:23:00 -0300

    Criar index.sh

---
+++ index.sh
@@ -0,0 +1,10 @@
+# 1. Atualizar o sistema e instalar dependências
+!apt update && !apt upgrade -y
+!pip install telethon
+
+# 2. Clonar o repositório
+!git clone https://github.com/AMHEEX/TLM-PY-BOT.git
+
+# 3. Entrar na pasta do projeto e executar
+%cd TLM-PY-BOT
+!python index.py
